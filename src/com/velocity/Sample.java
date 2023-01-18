49f67973d7868dd7c63bda8ec6e1a3834d824e1d
kanchan <kanchankcreative@gmail.com>
2023-01-18 22:45:33 +0530

    hello java

---
+++ src/com/velocity/Sample.java
@@ -0,0 +1,12 @@
+package com.velocity;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("java is simple  language ");
+		System.out.println("java is simple  language ");
+
+	}
+
+}
